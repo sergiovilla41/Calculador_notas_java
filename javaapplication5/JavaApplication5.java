50e59520bf51a9f2eed5330ea4ede36d215c759f
Sergio <serfusion41@gmail.com>
2023-09-09 18:37:03 -0500

    Primera version ensayo

---
+++ javaapplication5/JavaApplication5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author Trabajo
+ */
+public class JavaApplication5 {
+
+ 
+    public static void main(String[] args) {
+     int y=5;
+     int m=10;
+     int sum=y+m;
+        System.out.println(sum);
+     
+        
+    }
+    
+}
